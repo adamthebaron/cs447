6eed617b1fa989a0d2668ad1ab7b9b7109cc097e
adam <apk@pluvi.us>
2019-02-25 15:17:52 -0500

    in my younger days i used to sport a shaaaaaa

--- robotscan/RobotScan.java
+++ robotscan/RobotScan.java
@@ -16,7 +16,7 @@ class RobotScan extends Frame
 
 	RobotScan()
 	{
-		super("bargraph");
+		super("robot scan");
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e)
 			{
@@ -41,11 +41,13 @@ class CvRobotScan extends Canvas
 	int ycoords[];
 	int robotpos[] = new int[2];
 	int angle;
+	float angles[];
 	int numSensors;
 	float angleDiff;
 	Polygon poly;
-	int curx;
-	int cury;
+	float curx;
+	float cury;
+	float increment;
 
 	CvRobotScan()
 	{
@@ -77,6 +79,8 @@ class CvRobotScan extends Canvas
 			angle = Integer.parseInt(br.readLine());
 			System.out.println("robot heading at angle " + angle);
 			numSensors = Integer.parseInt(br.readLine());
+			angles = new float[numSensors];
+			angles[0] = 0f;
 			System.out.println(numSensors + " sensors");
 			System.out.println(numSensors + " scan lines:");
 			angleDiff = 2 * (float) Math.PI / numSensors;
@@ -98,22 +102,32 @@ class CvRobotScan extends Canvas
 		minx = 1;
 		miny = 1;
 
-		curx = robotpos[0];
-		cury = robotpos[1];
 		Graphics2D g2 = (Graphics2D) g;
 		// draw closed polygon
 		poly = new Polygon(xcoords, ycoords, n);
 		g2.draw(poly);
-
 		//draw robot
 		g2.drawRect(robotpos[0], robotpos[1], 10, 10);
+		increment = 0f;
 		// draw scanlines
-		while(poly.contains(curx, cury))
+		for(int i = 1; i < numSensors; i++)
 		{
-			curx += 1;
+			angles[i] = angles[i - 1] + angleDiff;
+			System.out.println("( " + i + ") angle is at " + angles[i]);
+			curx = robotpos[0] + 5;
+			cury = robotpos[1] + 5;
+			while(poly.contains(curx, cury))
+			{
+				increment += 0.001f;
+				curx += increment;
+				cury += increment;
+			}
+			curx -= increment;
+			cury -= increment;
+			curx += Math.cos(angleDiff);
+			cury += Math.sin(angleDiff);
+			System.out.println("boundary is at " + curx + " " + cury);
+			g2.drawLine(robotpos[0], robotpos[1], (int) curx, (int) cury);
 		}
-		curx -= 1;
-		System.out.println("boundary is at " + curx + " " + cury);
-		g2.drawLine(robotpos[0], robotpos[1], curx, cury);
 	}
 }
